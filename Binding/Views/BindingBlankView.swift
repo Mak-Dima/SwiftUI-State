f38b0672c6cffa770256d41f5eccc9aafc879bb4
maksiutenkodmytro <maksiutenko.dmytro88@gmail.com>
2026-02-16 13:20:00 -0600

    Binding feature directory added.

---
+++ Binding/Views/BindingBlankView.swift
@@ -0,0 +1,18 @@
+//
+//  BindingBlankView.swift
+//  SwiftUI-State
+//
+//  Created by Dmytro Maksiutenko on 2026-02-16.
+//
+
+import SwiftUI
+
+struct BindingBlankView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BindingBlankView()
+}
