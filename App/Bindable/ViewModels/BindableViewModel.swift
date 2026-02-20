56f5aeef0915d421ac6312df1af250eb030e5620
maksiutenkodmytro <maksiutenko.dmytro88@gmail.com>
2026-02-20 17:59:22 -0600

    Prepare data method added.

--- App/Bindable/ViewModels/BindableViewModel.swift
+++ App/Bindable/ViewModels/BindableViewModel.swift
@@ -8,5 +8,28 @@
 import Foundation
 
 @Observable class BindableViewModel {
+    var data: BindableModel
+    var ready: Bool = false
+    var isLoading: Bool = true
     
+    init(_ data: BindableModel) {
+        self.data = data
+    }
+    
+    func prepareData() async {
+        do {
+            try await Task.sleep(for: .seconds(5))
+            self.data = BindableModel(
+                id: UUID(),
+                name: "Test",
+                version: 1,
+                subversion: 0,
+                inUse: false
+            )
+            self.ready = true
+            self.isLoading = false
+        } catch {
+            self.isLoading = true
+        }
+    }
 }
