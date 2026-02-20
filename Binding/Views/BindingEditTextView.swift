3997cd400369bdfae9d0ec0de4db9038e7fec02b
maksiutenkodmytro <maksiutenko.dmytro88@gmail.com>
2026-02-19 18:15:17 -0600

    Animations and styles applied to the view.

--- Binding/Views/BindingEditTextView.swift
+++ Binding/Views/BindingEditTextView.swift
@@ -18,23 +18,42 @@ struct BindingEditTextView: View {
     var body: some View {
         VStack(alignment: .center, spacing: 20) {
             Text(parentData ?? "No Value")
+                .swingTextAnimationStartLeft()
             TextField("Enter Text", text: $draftData)
+                .background {
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.blue, lineWidth: 2)
+                }
+                .shadow(radius: 3)
                 .submitLabel(.return)
                 .onSubmit {
                     parentData = draftData
                     draftData.removeAll()
                 }
+                .pulseFrameAnimation(color: .blue, width: 360, height: 10)
 
             Text(parentValue.wrappedValue ?? "No Value")
+                .swingTextAnimationStartRight()
             TextField("Enter Text", text: $draftValue)
+                .background {
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.blue, lineWidth: 2)
+                }
+                .shadow(radius: 3)
                 .submitLabel(.return)
                 .onSubmit {
                     parentValue.wrappedValue = draftValue
                     draftValue.removeAll()
                 }
+                .pulseFrameAnimation(color: .blue, width: 360, height: 10)
             
         }
+        .ignoresSafeArea()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .padding()
+        .textFieldStyle(.roundedBorder)
+        .textCase(Text.Case.uppercase)
+        .background(Color.brown.mix(with: .blue, by: 0.5))
     }
 }
 
