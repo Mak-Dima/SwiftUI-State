83669b64b6b2a445507ae620f64a3eb40855f1ee
maksiutenkodmytro <maksiutenko.dmytro88@gmail.com>
2026-02-20 18:48:48 -0600

    Implemented initial view layout.

--- App/Bindable/Views/BindableBlankView.swift
+++ App/Bindable/Views/BindableBlankView.swift
@@ -8,11 +8,50 @@
 import SwiftUI
 
 struct BindableBlankView: View {
+    
+    @State var viewModel: BindableViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(alignment: .center, spacing: 20) {
+                HStack(alignment: .center, spacing: 20) {
+                    Spacer()
+                    NavigationLink("Edit") {
+                        
+                    }
+                }
+                .padding([.trailing], 40)
+                .padding([.top, .bottom], 80)
+                
+                Text(viewModel.data.id.uuidString)
+                Text(viewModel.data.name)
+                Text(String(viewModel.data.version))
+                Text(String(viewModel.data.subversion))
+                Text(viewModel.data.inUse ? "In use" : "Not in use")
+                
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .buttonStyle(.glass)
+            .buttonBorderShape(.roundedRectangle)
+            .background {
+                RadialGradient(colors: [.pink.mix(with: .red, by: 0.9), .indigo], center: .center, startRadius: 50, endRadius: 500)
+            }
+            .ignoresSafeArea()
+        }
     }
 }
 
 #Preview {
-    BindableBlankView()
+    @Previewable @State var vm = BindableViewModel(
+        BindableModel(
+            id: UUID(),
+            name: "Test",
+            version: 1,
+            subversion: 0,
+            inUse: false
+        )
+    )
+    
+    BindableBlankView(viewModel: vm)
 }
